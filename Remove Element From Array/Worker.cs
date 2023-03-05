ba70234afd4eefe9809d827d0a45021cbc2db27f
Ahmed Allam <Ahmed.Allam@esl.uk.com>
2023-03-05 17:12:59 +0200

    Add project files.

---
+++ Remove Element From Array/Worker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remove_Element_From_Array
+{
+    public class Worker
+    {
+        public Worker()
+        {
+            Work();
+        }
+        public void Work()
+        {
+            var nums = new int[] { 0, 1, 2, 2, 3, 0, 4, 2 };
+            RemoveElement(nums, 2);
+        }
+
+        public int RemoveElement(int[] nums, int val)
+        {
+            int index = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var current = nums[i];
+                if (current != val)
+                    nums[index++] = current;
+            }
+
+            Console.Write($"array without {val}: ");
+            for (int i = 0; i < index; i++)
+            {
+                Console.Write($"{nums[i]}, ");
+            }
+
+            return index;
+        }
+    }
+}
